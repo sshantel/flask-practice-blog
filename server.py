b34d3dd222fae0e06da76312496eef8b466531db
Chantel Yip <chantelyip422@gmail.com>
2020-05-23 20:29:45 -0700

    imported flask-wtf and created form and form submission result page

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 from forms import SignUpForm
 
 app = Flask(__name__) #create Flask application
@@ -14,9 +14,12 @@ def blog():
              {'title': 'Buying Sushi', 'author': 'Chantel'}]
     return render_template('blog.html', author="Chantel", cooking=False, posts=posts)
 
-@app.route('/signup')
+@app.route('/signup', methods =['GET','POST'])
 def signup():
     form = SignUpForm()
+    if form.is_submitted():
+        result = request.form
+        return render_template('user.html', result=result)
     return render_template('signup.html', form=form)
 
 if __name__ == '__main__':
