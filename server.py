32f1cb4dd1b87b4420689992912e7bdcee345fbf
Chantel Yip <chantelyip422@gmail.com>
2020-05-23 17:02:33 -0700

    initial commit

---
+++ server.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__) #create Flask application
+
+@app.route('/') #create route in server that's a single slash
+def home():
+    return 'Hello World'
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
